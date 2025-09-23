9a74613c5a61539144d0a39274c5b3e214dfe784
sdsouz12 <sdsouz12@asu.edu>
2025-09-22 21:50:18 -0700

    Add finite state machine setup

---
+++ Assets/Player_IdleState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player_IdleState : EntityState
+{
+    public Player_IdleState(Player player, StateMachine stateMachine, string stateName) : base(player, stateMachine, stateName)
+    {
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (player.moveInput.x != 0)
+            stateMachine.ChangeState(player.moveState);
+    }
+}
