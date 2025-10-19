0cc4502fbe84d5087a0bb14f8ae1a4a33c3280e1
Girish-123-dev <gnalawad@asu.edu>
2025-10-19 06:36:44 -0700

    Enemy creation completed

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class Enemy : Entity
+{
+    public Enemy_IdleState idleState;
+    public Enemy_MoveState moveState;
+    public Enemy_AttackState attackState;
+    public Enemy_BattleState battleState;
+
+    [Header("Battler Details")]
+    public float battleMoveSpeed = 3;
+    public float attackDistance = 2;
+    public float battleTimeDuration = 5;
+    public float minRetreatDistance = 1;
+    public Vector2 retreatVelocity;
+
+    //public float lastTimeWasInBattle;
+    //public float inGameTime;
+
+    [Header("Movement Details")]
+    public float idleTime = 2;
+    public float moveSpeed = 1.4f;
+    [Range(0,2)]
+    public float moveAnimSpeedMultiplier = 1;
+
+    [Header("Player detection")]
+    [SerializeField] private LayerMask whatIsPalyer;
+    [SerializeField] private Transform playerCheck;
+    [SerializeField] private float playerCheckDistance = 10;
+
+    //protected override void Update()
+    //{
+    //    base.Update();
+    //    inGameTime = Time.time;
+    //}
+
+    public RaycastHit2D PlayerDetected()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(playerCheck.position, Vector2.right * facingDir, playerCheckDistance, whatIsPalyer | whatIsGround);
+
+        if (hit.collider == null || hit.collider.gameObject.layer != LayerMask.NameToLayer("Player"))
+            return default;
+        
+        return hit;
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(playerCheck.position, new Vector3(playerCheck.position.x + (facingDir * playerCheckDistance), playerCheck.position.y));
+        Gizmos.color = Color.blue ;
+        Gizmos.DrawLine(playerCheck.position, new Vector3(playerCheck.position.x + (facingDir * attackDistance), playerCheck.position.y));
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(playerCheck.position, new Vector3(playerCheck.position.x + (facingDir * minRetreatDistance), playerCheck.position.y));
+
+
+    }
+}
---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+
+    protected StateMachine stateMachine;
+
+    private bool facingRight = true;
+    public int facingDir { get; private set; } = 1;
+
+
+    [Header("Collision detection")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] protected LayerMask whatIsGround;
+    [SerializeField] private Transform groundCheck;
+    public bool groundDetected { get; private set; }
+    public bool wallDetected;
+
+    protected virtual void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        stateMachine = new StateMachine();
+
+
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        HandleCollisionDetection();
+        stateMachine.UpdateActiveState();
+    }
+    public void CurrentStateAnimationTrigger()
+    {
+        stateMachine.currentState.AnimationTrigger();
+    }
+    public void SetVelocity(float xVelocity, float yVelocity)
+    {
+        rb.linearVelocity = new Vector2(xVelocity, yVelocity);
+        HandleFlip(xVelocity);
+
+    }
+    public void HandleFlip(float xVelcoity)
+    {
+        if (xVelcoity > 0 && facingRight == false)
+            Flip();
+        else if (xVelcoity < 0 && facingRight)
+            Flip();
+    }
+
+    public void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+        facingDir = facingDir * -1;
+    }
+
+    private void HandleCollisionDetection()
+    {
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, wallCheckDistance, whatIsGround);
+    }
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, groundCheck.position + new Vector3(0, -groundCheckDistance));
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(wallCheckDistance * facingDir, 0));
+
+    }
+}
